661bd8f4c5bf541d87ea498dd68149e180417fab
douglas.eduardo <douglas.eduardo@tbfg.com.br>
2024-04-17 12:20:48 -0300

    Adicionar arquivos de projeto.

---
+++ Models/MovimentoModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsultaMovimentoPDV.Models
+{
+    public class MovimentoModel
+    {
+        public Guid Id { get; set; }
+        public int Loja { get; set; }
+        public int Terminal { get; set; }
+        public string CPF_CNPJ { get; set; }
+        public decimal ValorTotal_Compra { get; set; }
+        public decimal ValorTotal_Item { get; set; }
+        public decimal Desconto { get; set; }
+        public int NR_Seq { get; set; }
+        public string Promocao { get; set; }
+        public string Barra { get; set; }
+        public string Produto { get; set; }
+    }
+}
---
+++ Models/PagamentoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsultaMovimentoPDV.Models
+{
+    public class PagamentoModel
+    {
+        public string MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }    
+    }
+}
